73405b467bb99e439a8fc4881bde1751d712884e
lkmartin90 <lucy.martin@ed.ac.uk>
2023-07-14 13:35:21 +0100

    Added code to for E57

---
+++ E57_drug_discovery/E57_DMSO_bootstrapping_LOPAC.py
@@ -0,0 +1,120 @@
+import sys
+sys.path.append("..")
+import pandas as pd
+from functions import *
+from random import choices
+
+########################################################
+## Import full data
+########################################################
+
+drug_data_tot = pd.read_csv('E57_LOPAC_full_data.csv')
+drug_data_tot["well_ending"] = drug_data_tot["Metadata_well"].str[1:]
+drug_data_tot["row"] = drug_data_tot["Metadata_well"].str[:1]
+drug_data_tot = drug_data_tot.set_index("ID")
+
+########################################################
+## Import senscore data
+########################################################
+
+all_data = pd.read_csv("E57_senscore_LOPAC.csv")
+
+all_data["ID"] = all_data["Metadata_platename"] + "_" + all_data["Metadata_well"]
+
+all_data["fraction_sen"] = all_data["number_sen"]/ all_data["cell_no"]
+
+all_data["conc"] = all_data["Metadata_platename"].map({'E57L458-0-5': 0.5, 'E57L458-3': 3, 'E57L459-0-5': 0.5, 'E57L459-3': 3,'E57L461-0-5': 0.5, 'E57L461-3': 3, 'E57L460-0-5': 0.5, 'E57L460-3': 3,})
+
+all_data["row"] = all_data["Metadata_well"].str[:1]
+
+all_data["well_ending"] = all_data["Metadata_well"].str[1:]
+
+# create a list of the compound names matching the compound IDs.
+compound = [list(drug_data_tot.loc[x]["compound"])[0] for x in all_data["ID"]]
+
+print(compound)
+# asign the compunds to a new column
+all_data["compound"] = compound
+drug_data_tot = drug_data_tot.reset_index()
+
+########################################################
+## Bootstrapping
+########################################################
+
+# Here want to only keep the DMSO columns
+
+DMSO_data = drug_data_tot.copy().loc[(drug_data_tot["compound"] == "DMSO")]
+
+# Then want to bootstrap from each DMSO well
+# For each well, want to take a subset of the cells, and take the mean of the senescence score
+
+all_dfs = []
+
+for plate in set(all_data["Metadata_platename"]):
+    print(plate)
+
+    # want to actually pool the DMSO wells on one plate
+    DMSO_on_plate = DMSO_data[(DMSO_data["Metadata_platename"] == plate)].copy()
+
+    num_DMSO_cells_on_plate = len(DMSO_on_plate)
+
+    # for each well on the plate
+    for well in set(drug_data_tot[(drug_data_tot["Metadata_platename"] == plate)]["Metadata_well"]):
+        print(well)
+        # find the compound in that well
+        compound = list(drug_data_tot[(drug_data_tot["Metadata_platename"] == plate) & (drug_data_tot["Metadata_well"] == well)]["compound"])[0]
+
+        # if not DMSO do the bootstrapping
+        if compound != "DMSO":
+
+            # find the number of cells in the well
+            num_cells_for_bootstrap = len(drug_data_tot[(drug_data_tot["Metadata_platename"] == plate) & (drug_data_tot["Metadata_well"] == well)])
+
+            # create empty arrays to store the summary data
+            subset_mean_senscore = []
+            subset_num_sencells = []
+            subset_std_senscore = []
+            subset_num_cells = []
+
+            #  Bootstrap 50 times, finding the mean of a subset of the DMSO cells
+
+            for i in range(50):
+                # chose random subset so cells, want this to be WITH replacement
+                chosen_cells = choices(DMSO_on_plate.index, k=num_cells_for_bootstrap)
+                chosen_cell_data = DMSO_on_plate.loc[chosen_cells]
+                #print(len(chosen_cell_data))
+
+                # find, mean, standard deviation, and number of senescent cells per well
+                mean_sen_score = np.mean(list(chosen_cell_data["sen_score"]))
+                std_sen_score = np.std(list(chosen_cell_data["sen_score"]))
+                tot_sen = sum(list(chosen_cell_data["sen_prediction"]))
+
+                # add to summary array
+                subset_num_cells.append(num_cells_for_bootstrap)
+                subset_mean_senscore.append(mean_sen_score)
+                subset_num_sencells.append(tot_sen)
+                subset_std_senscore.append(std_sen_score)
+
+
+            # create df of data from this well
+            subset_df = pd.DataFrame([subset_mean_senscore, subset_num_sencells, subset_std_senscore, subset_num_cells])
+            subset_df = subset_df.T
+            subset_df = subset_df.rename({0: 'mean_senscore', 1: 'num_sencells', 2: 'std_senscore', 3: 'num_cells'}, axis=1)
+
+
+            subset_df["ID"] = plate + "_" + well
+            all_dfs.append(subset_df)
+
+# create df with all data and read out to a file
+bootstrapped_data = pd.concat(all_dfs)
+
+print(bootstrapped_data.groupby("ID").std())
+# find the standard deviation of the bootstrapped means
+std_senscore_from_bootstrapping = bootstrapped_data.groupby("ID").std()["mean_senscore"]
+std_numsen_from_bootstrapping =  bootstrapped_data.groupby("ID").std()["num_sencells"]
+all_data = all_data.set_index("ID")
+all_data["boostrapped_senscore_mean_std"] = std_senscore_from_bootstrapping
+all_data["boostrapped_numcells_mean_std"] = std_numsen_from_bootstrapping
+
+print(all_data)
+all_data.to_csv("E57_LOPAC_with_bootstrap.csv")
---
+++ E57_drug_discovery/E57_LOPAC_from_pickle.py
@@ -0,0 +1,113 @@
+import sys
+sys.path.append("..")
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+################################################################################################
+## Test on drug discovery data
+#################################################################################################
+# load the data and the model
+drug_data_tot = pd.read_csv("E57_LOPAC_data.csv", index_col=False)
+drug_data_tot = drug_data_tot.drop("Unnamed: 0", axis = 1)
+
+clf_svm_2 = pickle.load(open('E57_SVM_model.sav', 'rb'))
+
+# AT this stage, before irrelevant features are deleted, we need to correct the intensity of different plates
+# against each other. To start with we will not correct features that will vary by cell density, as later cell no.
+# varies as drugs kill off cells, but correction would still be applied.
+
+LOPAC_meta = pd.read_csv("GCGR-Lpc-Metadata.csv")
+LOPAC_meta['Barcode'] = LOPAC_meta['Barcode'].str.replace('.', '-')
+drug_data_temp = drug_data_tot.copy()
+
+drug_data_temp = drug_data_temp.set_index(["Metadata_platename", "Metadata_well"])
+compound = (LOPAC_meta.set_index(['Barcode', "Well Name"])['chemical_name'])
+
+drug_data_temp["compound"] = compound
+drug_data_tot["compound"] = list(drug_data_temp["compound"])
+
+DMSO_grouped_means = pd.DataFrame(drug_data_tot[drug_data_tot["compound"] == "DMSO"].groupby(["Metadata_platename"]).mean())
+# DMSO_grouped_means now contains the mean of DMSO samples in 01 and 02 wells, for each plate
+plate_names = list(DMSO_grouped_means.index)
+
+print(DMSO_grouped_means)
+
+test_well = drug_data_tot[(drug_data_tot["Metadata_platename"] == "E57L458-0-5") & (drug_data_tot["Metadata_well"] == "B11")]["Intensity_MaxIntensity_W1"]
+test_well_corrected = (test_well.copy()/DMSO_grouped_means.loc[('E57L458-0-5')]["Intensity_MaxIntensity_W1"]) * DMSO_grouped_means.loc[plate_names[0]]["Intensity_MaxIntensity_W1"]
+
+# want to divide by the mean DMSO intensity for that plate, and multiply by the mean DMSO intensity for plate 1
+for measure in drug_data_tot.columns:
+    # split the column names at each "_"
+    split_measure = measure.split('_')
+    if split_measure[0] == "Intensity":
+        # if the column is an intensity measure then want to scale by the above masked metrics.
+        if split_measure[1][0] != 'S' and split_measure[1] != 'MassDisplacement':
+            for plate in plate_names:
+                drug_data_tot.loc[(drug_data_tot['Metadata_platename'] == plate), measure] = \
+                    drug_data_tot.loc[(drug_data_tot['Metadata_platename'] == plate), measure].copy() \
+                    / DMSO_grouped_means.loc[plate][measure] * DMSO_grouped_means.loc[plate_names[0]][measure]
+
+
+test_well_after = drug_data_tot[(drug_data_tot["Metadata_platename"] == "E57L458-0-5") & (drug_data_tot["Metadata_well"] == "B11")]["Intensity_MaxIntensity_W1"]
+print("normalisation test")
+print(test_well_corrected)
+print(test_well_after)
+
+# drop columns not needed for ML
+
+drug_data_reduced = drug_data_tot.copy().drop(["Metadata_platename", "Metadata_well", "ImageNumber", "ObjectNumber", "compound"], axis=1)
+
+# scale data
+# Implicit assumption here that we have the same proportion os senescent cells as in the training data? YES
+# Not sure that this is a problem if we use the relative senescence score?
+
+drug_data_scaled = StandardScaler().fit_transform(drug_data_reduced)
+
+# apply the classifier to the drug discovery data
+drug_pred = clf_svm_2.predict(drug_data_scaled)
+drug_pred_probs = clf_svm_2.decision_function(drug_data_scaled)
+
+# we produce a senescence score for each cell
+# want to scale that score by the minimum so that we have no negative values
+
+print("sen score of 0 mapped to ", min(drug_pred_probs))
+
+drug_data_tot["sen_score"] = drug_pred_probs + -1 * min(drug_pred_probs)
+
+drug_data_tot["sen_prediction"] = drug_pred
+
+drug_data_tot = drug_data_tot.dropna(axis=1)
+
+# group the data so we know what the senescence score is for each well of each plate
+
+grouped_dat = drug_data_tot.groupby(["Metadata_platename", "Metadata_well"])
+# find the mean cell senescence score per well
+mean_sen_score = grouped_dat.mean()["sen_score"]
+# find the standard deviation in senescence score per well
+std_sen_score = grouped_dat.std()["sen_score"]
+
+tot_sen = grouped_dat.sum()["sen_prediction"]
+
+index_sen_score = grouped_dat.mean().index
+
+#px.scatter(y=drug_pred_probs, x=np.arange(len(drug_pred_probs)))
+
+#px.scatter(y=mean_sen_score, error_y=std_sen_score, x=list([x[0] + "_" + x[1] for x in index_sen_score]))
+
+# find the number of cells in each well
+
+cell_no = pd.DataFrame(
+    drug_data_tot.groupby(["Metadata_platename", "Metadata_well", "ImageNumber"]).max()["ObjectNumber"].groupby(["Metadata_platename", "Metadata_well"]).sum())
+
+# create summary data for each well and save to file
+
+output_data = pd.DataFrame(mean_sen_score)
+
+output_data["std_sen_score"] = std_sen_score
+output_data["cell_no"] = cell_no
+output_data["number_sen"] = tot_sen
+
+output_data.to_csv('E57_senscore_LOPAC.csv')
+drug_data_tot["ID"] = drug_data_tot["Metadata_platename"] + "_" + drug_data_tot["Metadata_well"]
+drug_data_tot.to_csv('E57_LOPAC_full_data.csv')
